0935b6a85b5b2d3ea0b9df18bb698686debeb020
Mojgan Kavehei <mojgankavehei@USSFMKA-ML.local>
2017-01-24 13:19:37 -0800

    First Chai Mocha test

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var expect = require("chai").expect;
+
+describe ( "A basic test for mocha and chai", function() {
+    it("it should pass when everything is ok", function() {
+       expect(true).to.be.true;
+    });
+});
